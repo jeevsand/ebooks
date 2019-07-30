4cd55a5c1c714f29385583131ed4d0aa58497ff5
Jeevan Sandhu <JSandhu@spartaglobal.com>
2019-07-30 16:28:20 +0100

    connected to sql and did commands to extract books

---
+++ README.md
@@ -0,0 +1,18 @@
+#E-book Store pyodbc exercise 
+The following is to consolidate out understanding of pyodbc and Python.
+
+##Objectives 
+- Create a new database in SQL Server 
+- Insert about 5 books using SQL Server
+- Connect this project to that database 
+- Have a function that retrieves all books and print them with the following format:
+```
+title: XYZ - Author: XXPTO - Date: 1/3/2019
+```
+- Have a function that searches for a book's title
+- Be able to save one book 
+
+####A book should have:
+- Title 
+- Author 
+- Date 
---
+++ ebooks.py
@@ -0,0 +1,22 @@
+import pyodbc
+
+#Variables to connect to DB
+server = 'localhost,1433'
+database = 'Ebooks'
+username = 'SA'
+password = 'Passw0rd2018'
+
+docker_ebooks = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password)
+
+cursor = docker_ebooks.cursor()
+
+#Printing all the books
+all_books = cursor.execute("SELECT * FROM Books;")
+Counter = 1
+while True:
+    row_record = all_books.fetchone()
+    if row_record is None:
+        break
+    print(Counter, "Title: ",row_record.Title, "; Author: ", row_record.Author, "; Publish date: ", row_record.PublishDate)
+    Counter +=1
+
