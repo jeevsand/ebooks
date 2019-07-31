587b2cbded3d87f803c5c2f2c450e9e2512b4281
Jeevan Sandhu <JSandhu@spartaglobal.com>
2019-07-31 09:28:00 +0100

    saving the changes made after getting rid of errors

--- ebooks.py
+++ ebooks.py
@@ -11,12 +11,18 @@ docker_ebooks = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+
 cursor = docker_ebooks.cursor()
 
 #Printing all the books
-all_books = cursor.execute("SELECT * FROM Books;")
-Counter = 1
-while True:
-    row_record = all_books.fetchone()
-    if row_record is None:
-        break
-    print(Counter, "Title: ",row_record.Title, "; Author: ", row_record.Author, "; Publish date: ", row_record.PublishDate)
-    Counter +=1
+# all_books = cursor.execute("SELECT * FROM Books;")
+# Counter = 1
+# while True:
+#     row_record = all_books.fetchone()
+#     if row_record is None:
+#         break
+#     print(Counter, "Title: ",row_record.Title, "; Author: ", row_record.Author, "; Publish date: ", row_record.PublishDate)
+#     Counter +=1
+
+#Function to search for title
+def search_book(title):
+    cursor.execute("SELECT * FROM Books WHERE Title LIKE '" +title+"%'")
+    row = cursor.fetchone()
+    return row
 
---
+++ run_ebooks.py
@@ -0,0 +1,4 @@
+from ebooks import *
+
+
+print(search_book("Harry"))
